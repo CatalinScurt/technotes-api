19b4d9fb7ceefc0183cbda9e3eddbd7e527654c5
Catalin Scurt <cata_scurt@yahoo.com>
2023-02-13 17:42:49 +0200

    initial commit

---
+++ middleware/verifyJWT.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken')
+
+const verifyJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization || req.headers.Authorization
+
+    if (!authHeader?.startsWith('Bearer ')) {
+        return res.status(401).json({ message: 'Unauthorized' })
+    }
+
+    const token = authHeader.split(' ')[1]
+
+    jwt.verify(
+        token,
+        process.env.ACCESS_TOKEN_SECRET,
+        (err, decoded) => {
+            if (err) return res.status(403).json({ message: 'Forbidden' })
+            req.user = decoded.UserInfo.username
+            req.roles = decoded.UserInfo.roles
+            // console.log(req.roles)
+            // if (req.roles.includes('Admin'))
+            //     next()
+            // else res.status(403).json({ message: 'No access' })
+            next()
+        }
+    )
+}
+
+module.exports = verifyJWT 
